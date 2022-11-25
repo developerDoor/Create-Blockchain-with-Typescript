a168db9f5c2708cd7236f98cec86fb95c2580ce0
jhmoon <dothebestmoon1016@gmail.com>
2022-11-25 17:54:52 +0900

    first commit

---
+++ ex.ts
@@ -0,0 +1,14 @@
+let a : number = 1;
+let b : string = "il";
+let c : boolean = true
+
+const player : {
+    name : string,
+    age? : number
+} = {
+    name : "JH"
+}
+
+if(player.age && player.age < 10) {
+
+}
